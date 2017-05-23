f4154c947280e97c5b90fe970ea8a080866f1414
rijpma <auke.rijpma@gmail.com>
2017-05-23 14:21:03 +0200

    summarise model output

--- rolmodels.r
+++ rolmodels.r
@@ -5,6 +5,8 @@ library("e1071")
 library("nnet")
 library("randomForest")
 library("data.table")
+library("ROCR")
+library("xtable")
 
 source('rolfunctions.r')
 
@@ -50,37 +52,62 @@ sens = sum(man$manual, na.rm=T)
 bsl = matrix(c(spec, fane, fapo, sens), ncol=2)
 bsl
 
-keep = names(trn)[grep('correct|dist$|sdx|wife|mtchs|old|young|namefreq', names(trn))]
+keep = names(trn)[grep('correct|dist$|sdx|mtchs|both|namefreq', names(trn))]
+# print(xtable(data.frame(variable = keep, explanation = NA)), include.rownames=F)
+
 trn = trn[, keep, with=F]
 trn = trn[complete.cases(trn), ]
 apply(trn, 2, range)
 
-m_lgt = glm(correct ~ mlastdist + mfirstdist + minidist
-    + mlastsdx + mfirstsdx 
-    + wlastdist + wfirstdist # + winidist
-    # + wlastsdx + wfirstsdx
-    + wifeinboth + mtchs, # + old + young,
-    family=binomial,
-    data=trn)
-summary(m_lgt)
-pred_lgt_trn = predict(m_lgt, newdata=trn, type='response')
+m_lgt = glm(correct ~ ., family=binomial(link="logit"), data=trn)
 
+summary(m_lgt)
+
+texreg::texreg(m_lgt, label = "tab:logitmod", file = "logitmod.tex",
+    caption = "Logistical regression predicting record matches")
+pred_lgt_trn = predict(m_lgt, newdata=trn, type='response')
 fill = NULL
 cuts = seq(0.01, 0.99, length=1000)
 for (i in cuts){
     pred = m_lgt$fit > i
     fill = rbind(fill, modperf(pred, trn$correct))
 }
-
 fillr = fill / rowSums(fill)
 fill$cut = fillr$cut = cuts
-cutoff = cuts[which.min(fill[,3])]
-table(trn$correct, pred_lgt_trn > 0.5)
-table(trn$correct, pred_lgt_trn > 0.7)
 
+pdf("linkpaper/logitperformance.pdf", width=9, height=5)
+par(mfrow=c(1, 2))
+plot(cuts, fill$fapo / length(!trn$correct), type='l', col=2, ylab='error rate')
+lines(cuts, fill$fane / length(!trn$correct), type='l', col=2)
+abline(v = c(0.5, cuts[which.min(sqrt(fill[,2]^2 + fill[,3]^2))]), col='gray70')
+text(c(0.25, 0.7), c(0.06, 0.03), c("False positives", "false negatives"))
+
+# plot(cuts, sqrt(fill[,2]^2 + fill[,3]^2), type='l', col=2)
+perf = performance(prediction(pred_lgt_trn, trn$correct), measure="tpr", x.measure="fpr")
+plot(perf, col=2)
+
+# plot(cuts, fill$spec / sum(!trn$correct), type='l', col=2, ylim=c(0, 1))
+# lines(cuts, fill$sens / sum(trn$correct), type='l', col=2)
+dev.off()
+
+conf_lgt_trn = table(pred_lgt_trn > 0.5, trn$correct, dnn=c('predicted', 'true'))
 pred_lgt_vld = predict(m_lgt, newdata=vld, type='response')
-table(trn$correct, pred_lgt_trn > 0.7)
-table(vld$correct, pred_lgt_vld > 0.7)
+conf_lgt_vld = table(pred_lgt_vld > 0.5, vld$correct, dnn=c('predicted', 'true'))
+
+conf = cbind(conf_lgt_trn, conf_lgt_vld)
+conf = rbind(colnames(conf), conf)
+conf = cbind(rownames(conf), conf)
+conf = cbind(c("", "Predicted", ""), conf)
+print(xtable(conf,
+        caption = "Confusion matrix for logit models", 
+        label = "tab:lgt_cfm"), 
+    include.colnames=F, include.rownames=F,
+    hline.after=c(nrow(conf)),
+    add.to.row = list(pos=list(-1), command=c("& & \\multicolumn{4}{c}{Actual} \\\\ & & \\multicolumn{2}{c}{Train} & \\multicolumn{2}{c}{Test} \\\\ \\hline \\\\")),
+    file = "linkpaper/lgt_confmat.tex")
+
+conf_lgt_vld / rowSums(conf_lgt_vld)
+
 
 # m_svm = svm(as.factor(correct) ~ ., data=trn)
 # summary(m_svm)
@@ -107,14 +134,36 @@ table(vld$correct, pred_lgt_vld > 0.7)
 # suggests 3/4 for training, 5 for validation
 
 m_rf = randomForest(as.factor(correct) ~ ., data=trn, mty=5)
-summary(m_rf)
-varImpPlot(m_rf)
+
+pdf("linkpaper/coefplots.pdf", width=9, height=5)
+par(mfrow=c(1, 2), font.main=1)
+cf_lgt = coef(m_lgt)
+dotchart(sort(cf_lgt), main="Logistic reg.", xlab='reg. coef') 
+abline(v=0, col='gray70')
+
+varImpPlot(m_rf, main='Random forests')
+dev.off()
 
 m_rf # 
-pred_rf_trn = predict(m_rf, newdata=trn)
-table(trn$correct, pred_rf_trn)
-pred_rf_vld = predict(m_rf, newdata=vld)
-table(vld$correct, pred_rf_vld)
+pred_rf_trn = predict(m_rf, newdata=trn, type='prob')
+conf_rf_trn = table(trn$correct, pred_rf_trn[, 2] > 0.5)
+pred_rf_vld = predict(m_rf, newdata=vld, type='prob')
+conf_rf_vld = table(vld$correct, pred_rf_vld[, 2] > 0.5)
+
+conf = cbind(conf_rf_trn, conf_rf_vld)
+conf = rbind(colnames(conf), conf)
+conf = cbind(rownames(conf), conf)
+conf = cbind(c("", "Predicted", ""), conf)
+print(xtable(conf,
+        caption = "Confusion matrix for random forests model", 
+        label = "tab:rf_cfm"), 
+    include.colnames=F, include.rownames=F,
+    hline.after=c(nrow(conf)),
+    add.to.row = list(pos=list(-1), command=c("& & \\multicolumn{4}{c}{Actual} \\\\ & & \\multicolumn{2}{c}{Train} & \\multicolumn{2}{c}{Test} \\\\ \\hline \\\\")),
+    file = "linkpaper/rf_confmat.tex")
+
+conf_rf_trn / rowSums(conf_rf_trn)
+conf_rf_vld / rowSums(conf_rf_vld)
 
 # find vote optimum
 votes_vld = predict(m_rf, newdata=vld, type='prob')
@@ -123,41 +172,49 @@ fill = NULL
 for (share in voteshares){
     fill = rbind(fill, c(table(vld$correct, votes_vld[, 2] > share)))
 }
-colnames(fill) = c('ff', 'falseneg', 'falspos', 'tt')
-fill = fill / sum(vld$correct)
-matplot(voteshares, fill[, 2:3], lty=1, type='l', col=1)
-abline(v=0.5, col='gray', lty=2)
-text(x=voteshares[10], y=fill[5, 2:3] * 1.5, labels=c('falseneg', 'falsepos'))
+colnames(fill) = c('ff', 'fane', 'fapo', 'tt')
 
-plot(voteshares, sqrt(fill[,2]^2 + fill[,3]^2), type='l')
-abline(v=0.5, col='gray', lty=2)
+pdf("linkpaper/rfperformance.pdf", width=9, height=5)
+par(mfrow=c(1, 2))
+plot(voteshares, fill[, "fapo"] / length(!trn$correct), type='l', col=2, ylab='error rate', ylim=range(fill[, 2:3] / length(trn$correct)))
+lines(voteshares, fill[, "fane"] / length(!trn$correct), type='l', col=2)
+text(c(0.3, 0.7), c(0.025, 0.025), c("False positives", "false negatives"))
+abline(v = c(0.5, voteshares[which.min(sqrt(fill[,2]^2 + fill[,3]^2))]), col='gray70')
+
+# plot(voteshares, sqrt(fill[,2]^2 + fill[,3]^2), type='l', col=2)
+perf = performance(prediction(pred_rf_trn[, 2], trn$correct), measure="tpr", x.measure="fpr")
+plot(perf, col=2)
+
+# plot(voteshares, fill$spec / sum(!trn$correct), type='l', col=2, ylim=c(0, 1))
+# lines(voteshares, fill$sens / sum(trn$correct), type='l', col=2)
+dev.off()
 
 # do nowife on entire data with wife set to ''?
-m_rf_yeswf = randomForest(as.factor(correct) ~ ., data=trn[(wifepresent_from | wifepresent_to), ])
-m_rf_nowf = randomForest(as.factor(correct) ~ ., data=trn[!(wifepresent_from | wifepresent_to), ])
+# m_rf_yeswf = randomForest(as.factor(correct) ~ ., data=trn[(wifepresent_from | wifepresent_to), ])
+# m_rf_nowf = randomForest(as.factor(correct) ~ ., data=trn[!(wifepresent_from | wifepresent_to), ])
 
-m_rf
-m_rf_yeswf
-m_rf_nowf
+# m_rf
+# m_rf_yeswf
+# m_rf_nowf
 
-table(predict(m_rf_yeswf, newdata=trn[(wifepresent_from | wifepresent_to), ]), trn[(wifepresent_from | wifepresent_to), correct])
-table(predict(m_rf_nowf, newdata=trn[!(wifepresent_from | wifepresent_to), ]), trn[!(wifepresent_from | wifepresent_to), correct])
-table(predict(m_rf_yeswf, newdata=vld[(wifepresent_from | wifepresent_to),]), vld[(wifepresent_from | wifepresent_to), correct])
-table(predict(m_rf_nowf, newdata=vld[!(wifepresent_from | wifepresent_to),]), vld[!(wifepresent_from | wifepresent_to), correct])
-# improvement in wife present
-# deterioration if wife absent
+# table(predict(m_rf_yeswf, newdata=trn[(wifepresent_from | wifepresent_to), ]), trn[(wifepresent_from | wifepresent_to), correct])
+# table(predict(m_rf_nowf, newdata=trn[!(wifepresent_from | wifepresent_to), ]), trn[!(wifepresent_from | wifepresent_to), correct])
+# table(predict(m_rf_yeswf, newdata=vld[(wifepresent_from | wifepresent_to),]), vld[(wifepresent_from | wifepresent_to), correct])
+# table(predict(m_rf_nowf, newdata=vld[!(wifepresent_from | wifepresent_to),]), vld[!(wifepresent_from | wifepresent_to), correct])
+# # improvement in wife present
+# # deterioration if wife absent
 
-# proper check on comparable datasets suggests overall improvement if applied separtely
-# would still be better to incorporate this in model
-table(predict(m_rf, newdata=vld[(wifepresent_from | wifepresent_to),]), vld[(wifepresent_from | wifepresent_to), correct])
-table(predict(m_rf, newdata=vld[!(wifepresent_from | wifepresent_to),]), vld[!(wifepresent_from | wifepresent_to), correct])
-table(predict(m_rf_yeswf, newdata=vld[(wifepresent_from | wifepresent_to),]), vld[(wifepresent_from | wifepresent_to), correct])
-table(predict(m_rf_nowf, newdata=vld[!(wifepresent_from | wifepresent_to),]), vld[!(wifepresent_from | wifepresent_to), correct])
+# # proper check on comparable datasets suggests overall improvement if applied separtely
+# # would still be better to incorporate this in model
+# table(predict(m_rf, newdata=vld[(wifepresent_from | wifepresent_to),]), vld[(wifepresent_from | wifepresent_to), correct])
+# table(predict(m_rf, newdata=vld[!(wifepresent_from | wifepresent_to),]), vld[!(wifepresent_from | wifepresent_to), correct])
+# table(predict(m_rf_yeswf, newdata=vld[(wifepresent_from | wifepresent_to),]), vld[(wifepresent_from | wifepresent_to), correct])
+# table(predict(m_rf_nowf, newdata=vld[!(wifepresent_from | wifepresent_to),]), vld[!(wifepresent_from | wifepresent_to), correct])
 
-# does not perform well, may need tuning
-# m_nn = nnet(as.factor(correct) ~ ., data=trn, size=2)
-# summary(m_nn)
-# pred_nn_trn = predict(m_nn, type='class', newdata=trn)
-# table(trn$correct, pred_nn_trn)
-# pred_nn_vld = predict(m_nn, type='class', newdata=vld)
-# table(vld$correct, pred_nn_vld)
+# # does not perform well, may need tuning
+# # m_nn = nnet(as.factor(correct) ~ ., data=trn, size=2)
+# # summary(m_nn)
+# # pred_nn_trn = predict(m_nn, type='class', newdata=trn)
+# # table(trn$correct, pred_nn_trn)
+# # pred_nn_vld = predict(m_nn, type='class', newdata=vld)
+# # table(vld$correct, pred_nn_vld)
