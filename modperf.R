bdbbee0133c8c275284f446524b4d7e62f844af1
rijpma <auke.rijpma@gmail.com>
2017-09-06 20:45:21 +0200

    fix table orientation and titles + labels

--- modperf.R
+++ modperf.R
@@ -1,3 +1,7 @@
+rm(list=ls())
+
+setwd('~/dropbox/opgaafrol/')
+
 source('rolmodels.R')
 
 # logit model
@@ -18,7 +22,7 @@ fill$cut = fillr$cut = cuts
 
 pdf("linkpaper/logitperformance.pdf", width=9, height=5)
 par(mfrow=c(1, 2))
-plot(cuts, fill$fapo / length(!trn$correct), type='l', col=2, ylab='error rate')
+plot(cuts, fill$fapo / length(!trn$correct), type='l', col=2, xlab = 'threshold', ylab='error rate')
 lines(cuts, fill$fane / length(!trn$correct), type='l', col=2)
 abline(v = c(0.5, cuts[which.min(sqrt(fill[,2]^2 + fill[,3]^2))]), col='gray70')
 text(c(0.25, 0.7), c(0.06, 0.03), c("False positives", "false negatives"))
@@ -31,22 +35,23 @@ plot(perf, col=2)
 # lines(cuts, fill$sens / sum(trn$correct), type='l', col=2)
 dev.off()
 
-conf_lgt_trn = table(pred_lgt_trn > 0.5, trn$correct, dnn=c('predicted', 'true'))
+conf_lgt_trn = table(trn$correct, pred_lgt_trn > 0.5, dnn=c('actual', 'predicted'))
 pred_lgt_vld = predict(m_lgt, newdata=vld, type='response')
-conf_lgt_vld = table(pred_lgt_vld > 0.5, vld$correct, dnn=c('predicted', 'true'))
+conf_lgt_vld = table(vld$correct, pred_lgt_vld > 0.5, dnn=c('actual', 'predicted'))
 
 conf = cbind(conf_lgt_trn, conf_lgt_vld)
 conf = rbind(colnames(conf), conf)
 conf = cbind(rownames(conf), conf)
-conf = cbind(c("", "Predicted", ""), conf)
+conf = cbind(c("", "Actual", ""), conf)
 print(xtable(conf,
         caption = "Confusion matrix for logit models", 
         label = "tab:lgt_cfm"), 
     include.colnames=F, include.rownames=F,
     hline.after=c(nrow(conf)),
-    add.to.row = list(pos=list(-1), command=c("& & \\multicolumn{4}{c}{Actual} \\\\ & & \\multicolumn{2}{c}{Train} & \\multicolumn{2}{c}{Test} \\\\ \\hline \\\\")),
+    add.to.row = list(pos=list(-1), command=c("& & \\multicolumn{4}{c}{Predicted} \\\\ & & \\multicolumn{2}{c}{Train} & \\multicolumn{2}{c}{Test} \\\\ \\hline \\\\")),
     file = "linkpaper/lgt_confmat.tex")
 
+conf_lgt_trn / rowSums(conf_lgt_trn)
 conf_lgt_vld / rowSums(conf_lgt_vld)
 
 # random forests
@@ -56,38 +61,30 @@ par(mfrow=c(1, 2), font.main=1)
 cf_lgt = coef(m_lgt)
 dotchart(sort(cf_lgt), main="Logistic reg.", xlab='reg. coef') 
 abline(v=0, col='gray70')
-
 varImpPlot(m_rf, main='Random forests')
 dev.off()
 
 m_rf # 
 pred_rf_trn = predict(m_rf, newdata=trn, type='prob')
-conf_rf_trn = table(trn$correct, pred_rf_trn[, 2] > 0.5)
+conf_rf_trn = table(trn$correct, pred_rf_trn[, 2] > 0.5, dnn=c('actual', 'predicted'))
 pred_rf_vld = predict(m_rf, newdata=vld, type='prob')
-conf_rf_vld = table(vld$correct, pred_rf_vld[, 2] > 0.5)
+conf_rf_vld = table(vld$correct, pred_rf_vld[, 2] > 0.5, dnn=c('actual', 'predicted'))
 
-# tp = data.table(dist = vld$mlast_neighbour_lag_dist, pred = pred_rf_vld[,2 ])
-# plot(tp)
-# plot(predict(loess(pred ~ dist, data = tp), newdata = seq(0, 1, length.out = 1e3)))
-# tp = data.table(dist = vld$wmlastdist_from, pred = pred_rf_vld[,2 ])
-# plot(tp)
-# plot(predict(loess(pred ~ dist, data = tp), newdata = seq(0, 1, length.out = 1e3)))
-# plot(predict(m, newdata = data.frame(dist = seq(0, 1, length.out=1000))))
 # tp = data.table(dist = vld$minidist, pred = pred_rf_vld[,2 ])
 # plot(tp)
-# plot(predict(loess(pred ~ dist, data = tp), newdata = seq(0, 1, length.out = 1e3)))
-# lines(x=seq(0, 1, length.out=1e3), predict(m, newdata = data.frame(dist = seq(0, 1, length.out=1000))))
+# lines(seq(0, 1, length.out = 1e3),
+#     predict(loess(pred ~ dist, data = tp), newdata = seq(0, 1, length.out = 1e3)), col = 2)
 
 conf = cbind(conf_rf_trn, conf_rf_vld)
 conf = rbind(colnames(conf), conf)
 conf = cbind(rownames(conf), conf)
-conf = cbind(c("", "Predicted", ""), conf)
+conf = cbind(c("", "Actual", ""), conf)
 print(xtable(conf,
         caption = "Confusion matrix for random forests model", 
         label = "tab:rf_cfm"), 
     include.colnames=F, include.rownames=F,
     hline.after=c(nrow(conf)),
-    add.to.row = list(pos=list(-1), command=c("& & \\multicolumn{4}{c}{Actual} \\\\ & & \\multicolumn{2}{c}{Train} & \\multicolumn{2}{c}{Test} \\\\ \\hline \\\\")),
+    add.to.row = list(pos=list(-1), command=c("& & \\multicolumn{4}{c}{Predicted} \\\\ & & \\multicolumn{2}{c}{Train} & \\multicolumn{2}{c}{Test} \\\\ \\hline \\\\")),
     file = "linkpaper/rf_confmat.tex")
 
 conf_rf_trn / rowSums(conf_rf_trn)
@@ -104,7 +101,7 @@ colnames(fill) = c('ff', 'fane', 'fapo', 'tt')
 
 pdf("linkpaper/rfperformance.pdf", width=9, height=5)
 par(mfrow=c(1, 2))
-plot(voteshares, fill[, "fapo"] / length(!trn$correct), type='l', col=2, ylab='error rate', ylim=range(fill[, 2:3] / length(trn$correct)))
+plot(voteshares, fill[, "fapo"] / length(!trn$correct), type='l', col=2, xlab = 'threshold', ylab='error rate', ylim=range(fill[, 2:3] / length(trn$correct)))
 lines(voteshares, fill[, "fane"] / length(!trn$correct), type='l', col=2)
 text(c(0.3, 0.7), c(0.025, 0.025), c("False positives", "false negatives"))
 abline(v = c(0.5, voteshares[which.min(sqrt(fill[,2]^2 + fill[,3]^2))]), col='gray70')
@@ -122,63 +119,74 @@ false_positives_rf = cnd[smpl==0, ][pred_rf_vld[, 2] < 0.5 & correct == TRUE, ]
 false_positives_rf[, sum(wlast_to == "" | wlast_from == "")]
 false_positives_rf[, sum(wlast_to == "" & wlast_from == "")]
 false_positives_rf[, sum(wfirst_to == "")]
-# 15/31 had one wife missing, not the other ones
+# 2 were both missing, 16 had one or both wives missing
+false_positives_rf[(wlast_to == "" | wlast_from == ""), grep("(last|first)_", names(false_positives_rf)), with = F]
+# 1 wrong, 15 correct based on men's name only
+
 false_positives_rf[, grep("(last|first)_", names(false_positives_rf)), with = F]
 false_positives_rf[!(wlast_to == "" | wlast_from == ""), grep("(last|first)_", names(false_positives_rf)), with = F]
-# 12/15 remaining ones seem correct (3 of which have KRUGER surname (fourth most frequent)
-# 1/15 seems very likely (LABUSCHAGNE), but different m first name 
-# 1/15 seems very likely (LIEBENBERG), but different w surname 
-# 1/15 seems wrong (JANZEN)
+
+
+print(xtable(false_positives_rf[, lapply(.SD, tolower), .SDcols = grep("(last|first)_", names(false_positives_rf))],
+        caption = "False positives created by random forest classifier", 
+        label = "tab:falsepos"), 
+    include.colnames=T, include.rownames=F,
+    size="footnotesize",
+    floating.environment="sidewaystable",
+    file = "linkpaper/falsepositives.tex")
+# 1/9 seems wrong (KNOETZE), different wife surname
+# 1/9 is probably wrong due to different wife (BEETGE)
+
+# 3/9 seems very likely correct (LABUSCHAGNE, KRUGER), but slightly different m first name 
+# 1/9 seems likely correct (KRUGER), but different m first name 
+# 3/9 are probably correct
+
 
 # sensitivity to variable inclusion
 # base AUC
 base_auc = performance(prediction(as.numeric(predict(m_rf, vld)), vld$correct), 'auc')
-
+base_auc
 # commented out because slow
 # ml = list()
 # for (vrb in names(trn)[-3]){
 #     frm = as.formula(paste("factor(correct) ~ . -", vrb))
 #     ml[[vrb]] = randomForest(frm, data = trn)
-#     print(i)
+#     print(vrb)
 # }
 # perfs = sapply(ml, function(x) performance(prediction(as.numeric(predict(x, vld)), vld$correct), 'auc')@y.values)
 # dotchart(unlist(perfs))
-# print(xtable(data.table(names(trn)[-3], AUC = unlist(perfs))[order(AUC)],
-#         caption = "AUC after omitting one variable", 
+# sns1 = data.table(names(trn)[-3], AUC = unlist(perfs))[order(AUC)]
+# print(xtable(sns1,
+#         caption = "AUC after omitting one variable (full model AUC: 0.94)", 
 #         label = "tab:sens_1var"),
-#     file = "linkpaper/sensitivity_1var.tex", 
-#     type = 'latex', include.rownames=F)
+#     type = 'latex', include.rownames=F,
+#     file = "linkpaper/sensitivity_1var.tex")
 
 
-# ml = list()
-# ncomb = choose(length(names(trn)) - 1, 2)
-# combinations = combn(names(trn)[-3], 2)
-# for (i in 1:ncomb){
-#     frm = as.formula(paste("factor(correct) ~ . -", paste0(combinations[, i], collapse = ' - ')))
-#     ml[[i]] = randomForest(frm, data = trn)
-#     print(i)
-# }
-# perfs = sapply(ml, function(x) performance(prediction(as.numeric(predict(x, vld)), vld$correct), 'auc')@y.values)
-# dotchart(unlist(perfs))
-# plot(unlist(perfs))
-# plot(ecdf(unlist(perfs)))
-# abline(v = 0.91)
-# print(xtable(data.table(t(combinations[, perfs < 0.91]), AUC = unlist(perfs[perfs < 0.91]))[order(AUC)]),
-#     type = "latex", "linkpaper/sensitivity_2var.tex")
-# combinations[, perfs < 0.88]
+ml = list()
+sum(choose(length(names(trn)) - 1, 1:(length(names(trn)) - 1)))
+ncomb = choose(length(names(trn)) - 1, 2)
+combinations = combn(names(trn)[-3], 2)
+for (i in 1:ncomb){
+    frm = as.formula(paste("factor(correct) ~ . -", paste0(combinations[, i], collapse = ' - ')))
+    ml[[i]] = randomForest(frm, data = trn)
+    print(i)
+}
+perfs = sapply(ml, function(x) performance(prediction(as.numeric(predict(x, vld)), vld$correct), 'auc')@y.values)
+dotchart(unlist(perfs))
+plot(unlist(perfs))
+plot(ecdf(unlist(perfs)))
+abline(v = 0.931)
+out = data.table(t(combinations), AUC = unlist(perfs))[order(AUC), ]
+
+print(xtable(head(out, 20),
+    caption = "AUC after omitting two variables (full model AUC: 0.94). Note: only 20 lowest AUC values reported.", 
+    label = "tab:sens_2var"),
+    type = 'latex', include.rownames=F,
+    file = "linkpaper/sensitivity_2var.tex")
 
 
 # separate rf
-keep = names(trn)[grep('correct|[a-z]dist$|sdx|mtchs|wife|both|namefreq|dchild|spouse', names(trn))]
-
-trn = trn[, keep, with=F]
-trn = trn[complete.cases(trn), ]
-trn = trn[, lapply(.SD, normalise)]
-vld = vld[, lapply(.SD, normalise), .SDcols = names(trn)]
-apply(trn, 2, range, na.rm=T)
-apply(vld, 2, range, na.rm=T)
-
-
 m_rf
 m_rf_yeswf
 m_rf_nowf
@@ -190,10 +198,29 @@ table(predict(m_rf_nowf, newdata=vld[!(wifepresent_from | wifepresent_to),]), vl
 # improvement in wife present
 # deterioration if wife absent
 
-# proper check on comparable datasets suggests overall improvement if applied separtely
-# would still be better to incorporate this in model
+# proper check on comparable datasets suggests overall improvement if applied separately
+# would still be better to incorporate this in model directly
 table(predict(m_rf, newdata=vld[(wifepresent_from | wifepresent_to),]), vld[(wifepresent_from | wifepresent_to), correct])
 table(predict(m_rf_yeswf, newdata=vld[(wifepresent_from | wifepresent_to),]), vld[(wifepresent_from | wifepresent_to), correct])
 
 table(predict(m_rf, newdata=vld[!(wifepresent_from | wifepresent_to),]), vld[!(wifepresent_from | wifepresent_to), correct])
 table(predict(m_rf_nowf, newdata=vld[!(wifepresent_from | wifepresent_to),]), vld[!(wifepresent_from | wifepresent_to), correct])
+
+
+table(predict(m_rf, newdata=vld[(wifepresent_from | wifepresent_to),]), vld[(wifepresent_from | wifepresent_to), correct]) + 
+    table(predict(m_rf, newdata=vld[!(wifepresent_from | wifepresent_to),]), vld[!(wifepresent_from | wifepresent_to), correct])
+
+
+table(predict(m_rf_nowf, newdata=vld[!(wifepresent_from | wifepresent_to),]), vld[!(wifepresent_from | wifepresent_to), correct]) + 
+    table(predict(m_rf_yeswf, newdata=vld[(wifepresent_from | wifepresent_to),]), vld[(wifepresent_from | wifepresent_to), correct])
+
+
+
+m_rf_gnl # 
+pred_rf_trn_gnl = predict(m_rf_gnl, newdata=trn_gnl, type='prob')
+conf_rf_trn_gnl = table(trn_gnl$correct, pred_rf_trn_gnl[, 2] > 0.5, dnn=c('actual', 'predicted'))
+pred_rf_vld_gnl = predict(m_rf_gnl, newdata=vld_gnl, type='prob')
+conf_rf_vld_gnl = table(vld_gnl$correct, pred_rf_vld_gnl[, 2] > 0.5, dnn=c('actual', 'predicted'))
+
+conf_rf_trn_gnl / rowSums(conf_rf_trn_gnl)
+conf_rf_vld_gnl / rowSums(conf_rf_vld_gnl)
