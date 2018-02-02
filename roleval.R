6f2529dcf1680bbf1013f0dc33271cbb701d2bb5
rijpma <auke.rijpma@gmail.com>
2018-02-02 10:11:47 +0100

    linkrates table, namefrequencies, simpler panel plot

--- roleval.R
+++ roleval.R
@@ -56,21 +56,68 @@ fpopg = plm::pdata.frame(opg, index=c('rindex', 'year'))
 fpopg$lsettlerchildren = lag(fpopg$settlerchildren)
 fpopg$lcattle = lag(fpopg$cattle)
 
-pdf("linkpaper/panelresults.pdf")
-par(mfrow=c(2, 2), mar=mar)
+opg[, sum(rlen > 1), by = list(wifepresent)]
+links[, mean(len > 1), by = list(wifepresent)]
+links[, sum(wifepresent), by = len > 1]
+print(xtable(links[!is.na(namefreq), list(linkrate = sum(len > 1) / .N), by = # is.na(namefreq) is never linked
+        list(wifepresent, 
+            namefreq1 = data.table::between(namefreq, 1, 1), 
+            namefreq2_5 = data.table::between(namefreq, 2, 5),
+            namefreq6_10 = data.table::between(namefreq, 6, 10),
+            namefreq10plus = namefreq > 10)][order(wifepresent, namefreq1, namefreq2_5, namefreq6_10)],
+        caption = "Relative linking rates by presence of wife and standardised name frequency",
+        label = "tab:linkrates", digits = 2),
+    table.placement = "h!", 
+    file = "linkpaper/linkrates.tex")
+links[!is.na(namefreq), linkweight_wife_namefreq := 1 / (sum(len > 1) / .N), by = 
+    list(wifepresent, 
+        namefreq1 = data.table::between(namefreq, 1, 1), 
+        namefreq2_5 = data.table::between(namefreq, 2, 5),
+        namefreq6_10 = data.table::between(namefreq, 6, 10),
+        namefreq10plus = namefreq > 10)]
+links[!is.na(namefreq), linkweight_namefreq := 1 / (sum(len > 1) / .N), by = 
+    list(namefreq1 = data.table::between(namefreq, 1, 1), 
+        namefreq2_5 = data.table::between(namefreq, 2, 5),
+        namefreq6_10 = data.table::between(namefreq, 6, 10),
+        namefreq10plus = namefreq > 10)]
+links[!is.na(namefreq), linkweight_wifepres := 1 / (sum(len > 1) / .N), by = 
+    list(wifepresent)]
+plot(links[!is.na(namefreq), sum(len > 1) / .N, by = list(namefreq)][order(namefreq)], type = 'b', log = 'x')
+plot(links[, sum(len > 1) / .N, by = list(settlerchildren)][order(settlerchildren)], type = 'b', log = 'x')
+
+namefreqhist = hist(opg[, namefreq], plot = F, breaks = 30)
+namefreqdenst = density(links[, namefreq], na.rm = T, adjust = 4)
+
+pdf("linkpaper/namefrequencies.pdf")
+plot(namefreqdenst, log = 'x', col = 2,
+    main = "Kernel density of name frequencies", 
+    xlab = "Name frequncy")
+dev.off()
+pdf("linkpaper/linkrates_by_namefreq.pdf")
+plot(links[!is.na(namefreq), sum(len > 1) / .N, by = namefreq][order(namefreq)], 
+    type = 'b', log = 'x', col = 2, xlab = 'name frequency', ylab = 'linkage rate')
+dev.off()
+
+
+pdf("linkpaper/panelresults.pdf", height = 4)
+par(mfrow=c(1, 2), mar=mar)
 smoothScatter(popg$lsettlerchildren, popg$settlerchildren, 
     nbin=256, colramp=colorRampPalette(magma(256)),
-    xlab='lag(children)', ylab='children', main='Panel')
-smoothScatter(fpopg$lsettlerchildren, fpopg$settlerchildren, 
-    nbin=256, colramp=colorRampPalette(magma(256)),
-    xlab='lag(children)', ylab='children', main='Fake panel')
+    xlab='lag(children)', ylab='children')
+# smoothScatter(fpopg$lsettlerchildren, fpopg$settlerchildren, 
+#     nbin=256, colramp=colorRampPalette(magma(256)),
+#     xlab='lag(children)', ylab='children', main='Fake panel')
 smoothScatter(log1p(popg$cattle), log1p(popg$lcattle),
     nbin=256,  colramp=colorRampPalette(magma(256)),
     xlab='lag(log(cattle))', ylab='log(cattle)')
-smoothScatter(log1p(fpopg$cattle), log1p(fpopg$lcattle),
-    nbin=256,  colramp=colorRampPalette(magma(256)),
-    xlab='lag(log(cattle)', ylab='log(cattle')
+# smoothScatter(log1p(fpopg$cattle), log1p(fpopg$lcattle),
+#     nbin=256,  colramp=colorRampPalette(magma(256)),
+#     xlab='lag(log(cattle)', ylab='log(cattle')
 dev.off()
+
+cor(popg$lsettlerchildren, popg$settlerchildren, use = 'pairwise.complete')
+cor(log1p(popg$cattle), log1p(popg$lcattle), use = 'pairwise.complete')
+
 
 opg[, len := links$len]
 quantile(opg$len)
